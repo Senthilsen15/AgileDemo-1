c2e66d59f8c2e31c4c41459cad4f5728548db4c9
Kumar <anand.n.kumar@capgemini.com>
2017-09-15 14:45:03 +0530

    MyFirst gradle update

---
+++ MyFirstGradleProject/src/main/java/demo/MyApp.java
@@ -0,0 +1,10 @@
+package main.java.demo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello der, Hiope u r learning");
+	}
+
+}
